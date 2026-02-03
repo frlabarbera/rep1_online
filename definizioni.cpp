e7989f7133955d9203ff344277fb83a0ae9736bb
frlabarbera <fr.labarbera@gmail.com>
2026-02-03 14:42:52 +0100

    definisco filtra B

--- definizioni.cpp
+++ definizioni.cpp
@@ -6,7 +6,12 @@ using namespace std;
 
  void filtro_generale::filtra(int v){
       cout << v * 10;
-    }
+    };
+
+ void filtro_B::filtra(int v){
+    cout << v * 20;
+ };
+ 
 int main (){
     
 }
